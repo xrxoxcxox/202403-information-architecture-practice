94f568cf184a8655e175334a203eda64084a9a02
xrxoxcxox <watanuki.keisuke@a-tm.co.jp>
2024-03-22 13:19:13 +0900

    Create EpisodeCard component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { EpisodeCard } from './EpisodeCard'
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="grid min-h-svh place-items-center bg-slate-400">
+      <div className="grid w-[22.5rem]">
+        <EpisodeCard
+          title="Episode.5"
+          date="2024/01/05"
+          point={50}
+          isRead
+          isPurchased
+        />
+        <EpisodeCard
+          title="Episode.4"
+          date="2024/01/04"
+          point={50}
+          isPurchased
+        />
+        <EpisodeCard title="Episode.4" date="2024/01/03" point={50} />
+        <EpisodeCard title="Episode.2" date="2024/01/02" point={0} isRead />
+        <EpisodeCard title="Episode.1" date="2024/01/01" point={0} />
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ src/EpisodeCard.tsx
@@ -0,0 +1,41 @@
+type Props = {
+  title: string
+  date: string
+  point: number
+  isRead?: boolean
+  isPurchased?: boolean
+}
+
+export function EpisodeCard({
+  title,
+  date,
+  point,
+  isRead = false,
+  isPurchased = false,
+}: Props) {
+  const background = isRead ? 'bg-slate-200' : 'bg-white'
+  const pointColor =
+    point === 0 || isPurchased
+      ? 'border border-blue-600 text-blue-600'
+      : 'bg-blue-600 text-white'
+  const pointLabel = isPurchased ? 'Purchased' : `${point} pt`
+
+  return (
+    <div className={`grid grid-cols-3 gap-x-4 px-4 py-2 ${background}`}>
+      <img
+        src="https://via.placeholder.com/320x180"
+        alt=""
+        className="col-start-1 col-end-2 h-auto w-full rounded"
+      />
+      <div className="col-start-2 col-end-4 flex items-center">
+        <div className="flex flex-grow flex-col">
+          <span className="text-sm text-neutral-900">{title}</span>
+          <span className="text-xs text-neutral-500">{date}</span>
+        </div>
+        <span className={`rounded-full px-3 py-1 text-xs ${pointColor}`}>
+          {pointLabel}
+        </span>
+      </div>
+    </div>
+  )
+}
